65ee0e9a4337496a95e6d58172640416f31a9014
Rostyslav Klyba <rklyba@gmail.com>
2023-03-03 13:45:06 +0200

    Completed setting out Router in pre v. 6 way. Added some CSS styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
