5ae68424473b718342e9a7054fb99d998eeb4fd7
Rostyslav Klyba <rklyba@gmail.com>
2023-03-03 20:22:06 +0200

    Created components 'Faq', 'Contact', layout 'HelpLayout'. Set routing for them

---
+++ src/pages/help/Faq.jsx
@@ -0,0 +1,46 @@
+export default function Faq() {
+  return (
+    <div className="faq">
+      <h3>Frequently Asked Questions</h3>
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quo
+          inventore at? Assumenda, pariatur impedit.
+        </p>
+      </div>
+
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quo
+          inventore at? Assumenda, pariatur impedit.
+        </p>
+      </div>
+
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quo
+          inventore at? Assumenda, pariatur impedit.
+        </p>
+      </div>
+
+      <div className="question">
+        <p>
+          <strong>Lorem ipsum dolor sit amet.</strong>
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro quo
+          inventore at? Assumenda, pariatur impedit.
+        </p>
+      </div>
+    </div>
+  );
+}
