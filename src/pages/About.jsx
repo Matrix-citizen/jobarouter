31c71bf2fa6a0df054f1d3abc9055ede61f64758
Rostyslav Klyba <rklyba@gmail.com>
2023-03-05 14:23:41 +0200

    Inserted 'Navigate' component into 'About'. Completed the project

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 export default function About() {
+  const [user, setUser] = useState('mario')
+
+  if (!user) {
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="about">
       <h2>About us</h2>
@@ -20,6 +29,7 @@ export default function About() {
         assumenda voluptatum asperiores, quaerat suscipit laborum? Beatae
         consectetur incidunt temporibus quam.
       </p>
+      <button onClick={() => setUser(null)}>Logout</button>
     </div>
   );
 }
